762720edfed87b586ac388c4911788a25ddcb8b9
Jonathan Balsano <jrbalsano@gmail.com>
2013-10-05 01:05:01 -0400

    More clean things

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -2,7 +2,7 @@
 
 # Basic Setup
 sudo apt-get -y update
-sudo apt-get -y install curl
+sudo apt-get -y install curl git zsh
 
 # Postgres setup
 sudo apt-get -y install postgresql postgresql-client-common postgresql-client-9.1 pgadmin3
@@ -29,3 +29,8 @@ gem install rails --version 4.0.0 --no-ri --no-rdoc
 
 # Link the rails app to be in the home directory
 ln -fs /vagrant_weedout ~/weedout
+
+# Set up oh my zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+sed -i "s/robbyrussell/frisk/" ~/.zshrc
+sudo chsh -s /usr/bin/zsh vagrant
