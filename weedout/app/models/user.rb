350e37ec63e2b35b7a3b5b3b053de60009727374
Jonathan Balsano <jrbalsano@gmail.com>
2013-10-05 01:07:57 -0400

    Set up cleaner directory structure

---
+++ weedout/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+  before_validation :set_email
+
+  def set_email
+    if self.email == ""
+      self.email = "#{self.uni}@columbia.edu"
+    end
+  end
+end
