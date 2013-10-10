136c40437e05d091d49dd1eff554cad463172edc
Chris D'Angelo <cd2665@columbia.edu>
2013-10-09 23:07:10 -0400

    hard coded smtp settings for sending emails

--- weedout/config/environments/development.rb
+++ weedout/config/environments/development.rb
@@ -13,11 +13,27 @@ Weedout::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # DO care if mailer can't send
+  config.action_mailer.raise_delivery_errors = true
 
-  # Default action mailer config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+
+  # Suggested these details go in environtmental variabls
+  # http://stevechristie.tumblr.com/post/35158776548/how-to-set-up-local-environmental-variables
+  # setting up gmail for devise
+  # http://stevechristie.tumblr.com/post/35152797572/setting-up-the-mailer-for-devise
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "weedoutcolumbia",
+    password: "rubyongails"
+  }
+
+  # Using localhost 3030 because that's where vagrant port forwards to
+  config.action_mailer.default_url_options = { :host => 'localhost:3030' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
