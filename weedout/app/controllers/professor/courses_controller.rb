063f18bb288f57f965d6ad3e612a9c9eb6de5480
Chris D'Angelo <cd2665@columbia.edu>
2013-10-11 16:26:01 -0400

    some security for courses controller now

--- weedout/app/controllers/professor/courses_controller.rb
+++ weedout/app/controllers/professor/courses_controller.rb
@@ -1,5 +1,9 @@
 class Professor::CoursesController < ApplicationController
 	before_filter :authenticate_user!
+	# http://stackoverflow.com/questions/7411577/devise-before-filter-authenticate-admin
+	before_filter do
+		redirect_to new_user_session_path unless current_user && current_user.isprofessor
+	end
 
 	def index
 	  @courses = Course.all
