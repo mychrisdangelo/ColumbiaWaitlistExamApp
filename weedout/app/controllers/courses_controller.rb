c98030680c42ac6fd057ea2e7d7ba53b43ff46d7
Chris D'Angelo <cd2665@columbia.edu>
2013-10-11 00:46:13 -0400

    all logged in users brought to courses page

--- weedout/app/controllers/courses_controller.rb
+++ weedout/app/controllers/courses_controller.rb
@@ -1,5 +1,8 @@
 class CoursesController < ApplicationController
+	before_filter :authenticate_user!
+
 	def index
 	  @courses = Course.all
+	  @user = current_user
 	end
 end
--- weedout/config/routes.rb
+++ weedout/config/routes.rb
@@ -7,7 +7,7 @@ Weedout::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root "home#index"
+  root "courses#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
