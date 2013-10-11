44bdcb85d362d01adb803462df92250359fe7775
Chris D'Angelo <cd2665@columbia.edu>
2013-10-10 22:05:18 -0400

    example validation of professor working

--- weedout/app/controllers/application_controller.rb
+++ weedout/app/controllers/application_controller.rb
@@ -10,6 +10,5 @@ class ApplicationController < ActionController::Base
       super
     end
   end
-
-
 end
+
--- weedout/app/models/user.rb
+++ weedout/app/models/user.rb
@@ -1,9 +1,19 @@
+class ProfessorValidator < ActiveModel::Validator
+  def validate(record)
+    if record.signup_as_professor == true
+      record.errors[:base] << "Not a real professor."
+    end
+  end
+end
+
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   before_validation :set_email
+  validates_with ProfessorValidator
 
   def set_email
     if self.email == ""
@@ -15,3 +25,4 @@ class User < ActiveRecord::Base
     logger.info "testme was called"
   end
 end
+
