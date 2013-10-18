4aee7d848ebab057c47c99bcb2a55842b652eaaf
Jonathan Balsano <jrbalsano@gmail.com>
2013-10-18 14:25:45 -0400

    Fixed user editing bug (hopefully)

--- weedout/app/models/user.rb
+++ weedout/app/models/user.rb
@@ -1,4 +1,3 @@
-
 class ProfessorValidator < ActiveModel::Validator
   require 'csv' # Chris, this is for our CSV reader
   def isRealProfessor(record)
@@ -47,3 +46,13 @@ class User < ActiveRecord::Base
   end
 end
 
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+    private
+    def account_update
+        default_params.permit(:full_name, :signup_as_professor, :email, :password, :password_confirmation, :current_password)
+    end
+
+    def sign_up
+        default_params.permit(:uni, :full_name, :signup_as_professor, :email, :password, :password_confirmation, :current_password)
+    end
+end
