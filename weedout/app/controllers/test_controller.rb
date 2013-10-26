13a0d9b7d07d825cdf8a592faa0a90dd280477b8
Chris D'Angelo <cd2665@columbia.edu>
2013-10-25 23:29:17 -0400

    grade list now showing in test/show

--- weedout/app/controllers/test_controller.rb
+++ weedout/app/controllers/test_controller.rb
@@ -26,6 +26,31 @@ class TestController < ApplicationController
   end
 
   def show
+    @grade_list = Hash.new
     @course = params[:course]
+    # first check all responses for this course
+    questions = Course.find(@course).questions
+    @first_question = questions.first
+
+    @responses_fq = @first_question.question_responses
+    @responses_fq.each do |r|
+      u = r.uni
+      @grade_list[u] = Array.new(11)
+      @grade_list[u][10] = 0
+      i = 0
+      responses_for_this_uni = Response.where(uni: u)
+      responses_for_this_uni.each do |r_uni|
+        if r_uni.question_choice_id == r_uni.question.correct_choice_id then
+          @grade_list[u][i] = 1
+          @grade_list[u][10] = @grade_list[u][10] + 1
+        else
+          @grade_list[u][i] = 0
+        end
+        i = i + 1
+      end
+    end
+
+
+
   end
 end
