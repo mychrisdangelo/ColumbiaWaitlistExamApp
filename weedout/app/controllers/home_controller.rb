0d09a2c482411e49d12fdd782567b0c3c4033944
Jonathan Balsano <jrbalsano@gmail.com>
2013-10-30 00:52:52 -0400

    Added bootstrap and style changes

--- weedout/app/controllers/home_controller.rb
+++ weedout/app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
   	if user_signed_in? && current_user.isprofessor == true
   		redirect_to professor_courses_path
-  	else
+  	elsif user_signed_in?
   		redirect_to courses_path
   	end 
   end
