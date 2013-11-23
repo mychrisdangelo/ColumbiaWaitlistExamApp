80983a44a8430ce0f4d8d8852e05bbd77bfb771c
Jonathan Balsano <jrbalsano@gmail.com>
2013-11-23 15:40:38 -0500

    Began implementing testing frameworks

---
+++ weedout/spec/controllers/professor/courses_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Professor::CoursesController do
+  context "when no courses are in the Courses API" do
+    it "should return no results" do
+      pending "figuring out how to test this"
+    end
+  end
+
+end
